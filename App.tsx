3d777226c0ed2af6d65469d502fc69a089949dbd
Fatih Canbey <fcanbeyy@gmail.com>
2022-02-24 01:33:07 +0300

    config eslint, prettier husky

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, Text, Box } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NativeBaseProvider>
+        <Box flex={1} bg="#000000" alignItems="center" justifyContent="center">
+          <Text>Open up App.js to start working on your app!</Text>
+        </Box>
+      </NativeBaseProvider>
+      <ExpoStatusBar />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
