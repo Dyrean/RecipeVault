3e494c59e8e704e2d663e86e2463462df19371eb
Fatih Canbey <fcanbeyy@gmail.com>
2022-02-24 03:43:59 +0300

    absolute paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@/screen': 'src/screen',
+            '@/components': 'src/components',
+            '@/navigation': 'src/navigation',
+            '@/hooks': 'src/hooks',
+            '@/styles': 'src/styles',
+            '@/services': 'src/services',
+            '@/types': 'src/types',
+          },
+        },
+      ],
+    ],
   };
 };
